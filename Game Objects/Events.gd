8ebedb67b1db8b60b91c41f8893b77066182b6cb
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-12 11:41:13 -0700

    last bit of lil changes

--- Game Objects/Events.gd
+++ Game Objects/Events.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_body_entered(body):
-	queue_free()
-	var hearts=get_tree().get_nodes_in_group("Heart")
-	Events.
-	print(hearts.size())
-	
