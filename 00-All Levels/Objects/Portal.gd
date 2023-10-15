5b946dbd334a23e27fea184ca7639e78eca3247a
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-14 20:30:16 -0700

    Changed Scene Transfer Portal Script to not need specific level names

--- 00-All Levels/Objects/Portal.gd
+++ 00-All Levels/Objects/Portal.gd
@@ -1,9 +1,12 @@
 extends Area2D
-
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() + 1
+		var current_scene_number = current_scene_file.to_int()
+		if current_scene_number == 10:
+			var next_level_path = "res://02-Lust/Level/level_1.tscn"
+			get_tree(). change_scene_to_file(next_level_path)
+		elif current_scene_number == 21:
+			var next_level_path = "res://02-Lust/Level/level_2.tscn"
+			get_tree(). change_scene_to_file(next_level_path)
 		
-		var next_level_path = "res://Levels/level_" + str(next_level_number) + ".tscn"
-		get_tree(). change_scene_to_file(next_level_path)
