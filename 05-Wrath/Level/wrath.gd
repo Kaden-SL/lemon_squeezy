a3753b16d1593bc8a6ae0f911571ce390073e39c
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-16 13:05:25 -0700

    Added basic music playing to wrath

--- 05-Wrath/Level/wrath.gd
+++ 05-Wrath/Level/wrath.gd
@@ -3,10 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	Events.wrathstart.connect(music_start)
 	
 	
-	
+func music_start():
+	$wrathmusic.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- 05-Wrath/Player/WrathPlayer.gd
+++ 05-Wrath/Player/WrathPlayer.gd
@@ -11,7 +11,7 @@ func _ready():
 	Dialogic.connect('timeline_ended',dialogue_done)
 func dialogue_done():
 	can_move=true
-	
+	Events.wrathstart.emit()
 func _physics_process(delta):
 	if can_move:
 		self.velocity = direction * speed
