9c4d1bba499464f5cfbe8ee8bea93750a28104b5
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-16 11:20:26 -0700

    Got my wrath level movement working

--- 05-Wrath/Player/WrathPlayer.gd
+++ 05-Wrath/Player/WrathPlayer.gd
@@ -1,48 +1,33 @@
 extends CharacterBody2D
 
-
-@export var WALK_FORCE = 1000
-const WALK_MAX_SPEED = 125
-const STOP_FORCE = 900
-const GRAVITY = 400.0
-const JUMP_VELOCITY = 200
-
-var walk = 0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
+@export var speed = 400
+var can_move = true
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-		
-	
-	walk = WALK_FORCE * (Input.get_action_strength("right") - Input.get_action_strength("left"))
-	
-	
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = -JUMP_VELOCITY
+	if can_move:
+		check_input()
 
-	if Input.is_action_just_released("ui_accept"):
-		jump_cut()
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	
-	if direction > 0 or direction < 0:
-		$PlayerSprite.scale.x = direction
-	
-	if direction:
-		velocity.x += walk * delta
-	else:
-		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
-
-	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
-	
+	# Use move_and_slide for movement
 	move_and_slide()
 
-func jump_cut():
-	if velocity.y < -100:
-		velocity.y = -100
+	# Reset velocity if a wall is hit
+	if is_on_wall() or is_on_floor() or is_on_ceiling():
+		velocity = Vector2.ZERO
+		can_move = true
+func check_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('right'):
+		velocity.x = 1
+	elif Input.is_action_pressed('left'):
+		velocity.x = -1
+	elif Input.is_action_pressed('up'):
+		velocity.y = -1
+	elif Input.is_action_pressed('down'):
+		velocity.y = 1
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * speed
+		can_move = false
+
+func _on_wall_collision() -> void:
+	self.velocity = Vector2.ZERO
+	can_move = true
