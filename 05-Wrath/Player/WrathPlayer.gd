090a1adb2ef22bbc9619bdd364095ebbb4a84b99
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-16 12:14:42 -0700

    got better wrath movement working

--- 05-Wrath/Player/WrathPlayer.gd
+++ 05-Wrath/Player/WrathPlayer.gd
@@ -1,33 +1,32 @@
 extends CharacterBody2D
 
 @export var speed = 400
-var can_move = true
+var direction : Vector2 = Vector2.ZERO
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and direction == Vector2.ZERO:
+		direction = (event.global_position - global_position).normalized()
 
 func _physics_process(delta):
-	if can_move:
-		check_input()
-
-	# Use move_and_slide for movement
+	self.velocity = direction * speed
 	move_and_slide()
 
-	# Reset velocity if a wall is hit
 	if is_on_wall() or is_on_floor() or is_on_ceiling():
-		velocity = Vector2.ZERO
-		can_move = true
-func check_input():
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('right'):
-		velocity.x = 1
-	elif Input.is_action_pressed('left'):
-		velocity.x = -1
-	elif Input.is_action_pressed('up'):
-		velocity.y = -1
-	elif Input.is_action_pressed('down'):
-		velocity.y = 1
-	if velocity != Vector2.ZERO:
-		velocity = velocity.normalized() * speed
-		can_move = false
+		direction = Vector2.ZERO
 
-func _on_wall_collision() -> void:
-	self.velocity = Vector2.ZERO
-	can_move = true
+#func check_input():
+#	velocity = Vector2.ZERO
+#	if Input.is_action_pressed('right'):
+#		velocity.x = 1
+#	elif Input.is_action_pressed('left'):
+#		velocity.x = -1
+#	elif Input.is_action_pressed('up'):
+#		velocity.y = -1
+#	elif Input.is_action_pressed('down'):
+#		velocity.y = 1
+#	if velocity != Vector2.ZERO:
+#		velocity = velocity.normalized() * speed
+#		can_move = false
+
+#func _on_wall_collision() -> void:
+#	self.velocity = Vector2.ZERO
+#	can_move = true
