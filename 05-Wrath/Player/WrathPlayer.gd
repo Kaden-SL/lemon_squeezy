32cd4b951586fa0d7548a990e9c71564e97a51a7
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-18 20:02:07 -0700

    jesus fucking christ I fixed my movment

--- 05-Wrath/Player/WrathPlayer.gd
+++ 05-Wrath/Player/WrathPlayer.gd
@@ -22,20 +22,4 @@ func _physics_process(delta):
 		if is_on_wall() or is_on_floor() or is_on_ceiling():
 			direction = Vector2.ZERO
 
-#func check_input():
-#	velocity = Vector2.ZERO
-#	if Input.is_action_pressed('right'):
-#		velocity.x = 1
-#	elif Input.is_action_pressed('left'):
-#		velocity.x = -1
-#	elif Input.is_action_pressed('up'):
-#		velocity.y = -1
-#	elif Input.is_action_pressed('down'):
-#		velocity.y = 1
-#	if velocity != Vector2.ZERO:
-#		velocity = velocity.normalized() * speed
-#		can_move = false
 
-#func _on_wall_collision() -> void:
-#	self.velocity = Vector2.ZERO
-#	can_move = true
