b62f0a8d9cc6a9c3ef9689ad4af33aca96400459
Kaden-SL <ksedmakl@ucsc.edu>
2023-10-18 20:50:42 -0700

    Stitching levels together

--- 00-All Levels/Objects/Portal.gd
+++ 00-All Levels/Objects/Portal.gd
@@ -1,8 +1,10 @@
 extends Area2D
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var current_scene_number = current_scene_file.to_int()
+		print(current_scene_number)
 		if current_scene_number == 10:
 			var next_level_path = "res://02-Lust/Level/level_1.tscn"
 			get_tree(). change_scene_to_file(next_level_path)
@@ -11,5 +13,8 @@ func _on_body_entered(body):
 			get_tree(). change_scene_to_file(next_level_path)
 		elif current_scene_number == 22:
 			var next_level_path = "res://04-Greed/Level/greed.tscn"
+			get_tree(). change_scene_to_file(next_level_path)
+		elif current_scene_number == 5:
+			var next_level_path = "res://06-Heresy/Level/heresy.tscn"
 			get_tree(). change_scene_to_file(next_level_path)
 		
--- 04-Greed/Level/greed.gd
+++ 04-Greed/Level/greed.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Dialogic.start('greed')
-
+	Dialogic.start('Greed_timeline')
+	Dialogic.connect('timeline_ended',dialogue_done)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func dialogue_done():
+	get_tree(). change_scene_to_file('res://05-Wrath/Level/wrath.tscn')
